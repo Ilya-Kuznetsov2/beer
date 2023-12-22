097883390641e54de4f29bc917f704acf13e60fb
Ilya Kuznetsov <99133715+Ilya-Kuznetsov2@users.noreply.github.com>
2023-12-22 07:24:09 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import psycopg2
+from tkinter import *
+from tkinter import ttk, messagebox
+from PIL import Image, ImageTk
+import types
+from functools import partial
+
+from interface import window_initializing as w_i
+
+
+
+
+
+window = w_i.window
+
+
+
+
+window.mainloop()
